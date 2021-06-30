81497430a138683b6f514ea6a5a9568849b27df4
Yazmin Rodriguez <79778345+Yazz-01@users.noreply.github.com>
2021-06-29 23:30:02 -0500

    Update plots.js

---
+++ plots.js
@@ -0,0 +1,18 @@
+// Reading the json file with d3
+d3.json("data/samples.json").then((bellyData) => {
+            window.bellyData = bellyData;
+            console.log(bellyData);
+            var data = bellyData;
+
+            // Add IDs to the DropDown menu
+            var idList = data.names;
+            for (i = 0; i < idList.length; i++) {
+                selectBox = d3.select("#selDataset");
+                selectBox = append("option").text(idList[i]);
+
+            }
+
+
+
+
+            Plotly.newPlot('myDiv', data);
